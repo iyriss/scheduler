427836a4f8314341789941ab5c3ad0a91a15b69c
Iyris Porras <iyrisporras@Iyriss-MacBook-Pro-2.local>
2020-09-29 16:14:50 -0700

    refactor interviewer components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import InterviewerListItem from 'components/InterviewerListItem';
 
-const InterviewerList = ({interviewers, setInterviewer, interviewer}) => {
+const InterviewerList = (props) => {
   // console.log(props)
-  const interviews = interviewers.map(person => {
+  const interviews = props.interviewers.map(person => {
     return (
       <InterviewerListItem
         key={person.id}
         id={person.id}
         name={person.name}
         avatar={person.avatar} 
-        selected={person.id ===  interviewer}
-        setInterviewer={setInterviewer}/>
+        selected={person.id ===  props.value}
+        // setInterviewer={setInterviewer}
+        setInterviewer={(event) => props.onChange(person.id)}/>
     )
   })
   return (
@@ -23,3 +24,26 @@ const InterviewerList = ({interviewers, setInterviewer, interviewer}) => {
 };
 
 export default InterviewerList;
+
+//destructuring:
+// const InterviewerList = ({interviewers, setInterviewer, interviewer}) => {
+//   // console.log(props)
+//   const interviews = interviewers.map(person => {
+//     return (
+//       <InterviewerListItem
+//         key={person.id}
+//         id={person.id}
+//         name={person.name}
+//         avatar={person.avatar} 
+//         selected={person.id ===  interviewer}
+//         // setInterviewer={setInterviewer}
+//         setInterviewer={(event) => setInterviewer(person.id)}/>
+//     )
+//   })
+//   return (
+//     <section className="interviewers">
+//       <h4 className="interviewers__header text--light">Interviewers</h4>
+//       <ul className="interviewers__list">{interviews}</ul>
+//     </section>
+//   );
+// };
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -11,7 +11,7 @@ const InterviewerListItem = (props) => {
   return (
     <li 
       className={interviewer}
-      onClick={() => props.setInterviewer(props.id)}>
+      onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
