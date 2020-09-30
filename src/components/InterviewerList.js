71ab6f8dd6bb65c93814ab6192cead18d727f8be
Iyris Porras <iyrisporras@Iyriss-MacBook-Pro-2.local>
2020-09-29 21:19:04 -0700

    add Appointment form tracking state

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import InterviewerListItem from 'components/InterviewerListItem';
+import 'components/InterviewerList.scss';
 
 const InterviewerList = (props) => {
   // console.log(props)
   const interviews = props.interviewers.map(person => {
     return (
       <InterviewerListItem
+
         key={person.id}
         id={person.id}
         name={person.name}
         avatar={person.avatar} 
         selected={person.id ===  props.value}
         // setInterviewer={setInterviewer}
-        setInterviewer={(event) => props.onChange(person.id)}/>
+        setInterviewer={props.onChange}/>
     )
   })
   return (
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,3 +156,5 @@ storiesOf("Appointment", module)
   />)
   .add("Status", () => <Status message="Deleting" />)
   .add("Error", () => <Error message="Could not delete appointment⚠️" onClose={action("onClose")} />)
+  .add("Edit", () => <Form name="Archie Cohen" interviewers={interviewers} value={1} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
