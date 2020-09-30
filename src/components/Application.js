99f2a997505175889e15076426fbe41217390512
Iyris Porras <iyrisporras@Iyriss-MacBook-Pro-2.local>
2020-09-30 13:04:36 -0700

    render the schedule, still static

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
 
 const days = [
   {
@@ -21,8 +23,63 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Archie Cohen",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "10am",
+  },
+  {
+    id: 5, 
+    time: "4pm",
+    interview: {
+      student: "Maria Boucher",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const meetings = appointments.map((appointment) => {
+    return (
+      <Appointment
+      key={appointment.id}
+      {...appointment}
+      />
+    )
+  })
   return (
     <main className="layout">
       <section className="sidebar">
@@ -38,8 +95,10 @@ export default function Application(props) {
       <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {meetings}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
+
