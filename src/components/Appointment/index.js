a59a66bee9c91089a1542ce8b404cdfa3c34f945
Iyris Porras <iyrisporras@Iyriss-MacBook-Pro-2.local>
2020-09-30 09:06:06 -0700

    build Appointment booked and Appointment empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from 'react'
 import "./styles.scss"
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
-const Appointment = () => {
+const Appointment = (props) => {
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>}
+    </article>
+  
   );
 };
 
