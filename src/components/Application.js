ad59720c21e488bce03b227059491de1ea66ce80
Iyris Porras <iyrisporras@Iyriss-MacBook-Pro-2.local>
2020-10-01 12:43:20 -0700

    use promise.all to run promises concurrently and grab data from the api-server and set it in the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,64 +4,20 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "../helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Archie Cohen",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcom",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "10am",
-  },
-  {
-    id: 5, 
-    time: "4pm",
-    interview: {
-      student: "Maria Boucher",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  }
-];
 
 export default function Application(props) {
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days })); //prevent make the request every time component renders
+  // const setDays = days => setState(prev => ({ ...prev, days })); //prevent make the request every time component renders
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // appointments: {}
+    appointments: {}
   });
 
-  const meetings = appointments.map((appointment) => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const meetings = dailyAppointments.map((appointment) => {
     return (
       <Appointment
       key={appointment.id}
@@ -71,9 +27,16 @@ export default function Application(props) {
   })
 
   useEffect(() => {
-    const URL = "/api/days"
-    axios.get(URL).then (response => {
-      setDays(response.data);
+    const URLdays = "/api/days"
+    const URLappointments = "/api/appointments"
+    Promise.all([
+      axios.get(URLdays),
+      axios.get(URLappointments)
+    ]).then((response) => {
+      setState(prev => ({...prev, 
+        days: response[0].data, 
+        appointments: response[1].data,
+       }));
     })
   }, []);
   return (
